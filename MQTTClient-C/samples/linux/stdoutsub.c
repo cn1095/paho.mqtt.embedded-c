8fdf7f55a3fde0ca62f26bc90d2906068d247b32
cn1095 <1095053105@qq.com>
2025-02-21 18:21:54 +0800

    Update stdoutsub.c

--- MQTTClient-C/samples/linux/stdoutsub.c
+++ MQTTClient-C/samples/linux/stdoutsub.c
@@ -1,16 +1,58 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2016 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution. 
+ *
+ * The Eclipse Public License is available at 
+ *   http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at 
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial contribution
+ *    Ian Craggs - change delimiter option from char to string
+ *    Al Stockdill-Mander - Version using the embedded C client
+ *    Ian Craggs - update MQTTClient function names
+ *******************************************************************************/
+
+/*
+ 
+ stdout subscriber
+ 
+ compulsory parameters:
+ 
+  topic to subscribe to
+ 
+ defaulted parameters:
+ 
+	--host localhost
+	--port 1883
+	--qos 2
+	--delimiter \n
+	--clientid stdout_subscriber
+	
+	--userid none
+	--password none
+
+ for example:
+
+    stdoutsub topic/of/interest --host iot.eclipse.org
+
+*/
 #include <stdio.h>
 #include <memory.h>
-#include <pthread.h>
 #include "MQTTClient.h"
 
 #include <stdio.h>
 #include <signal.h>
+
 #include <sys/time.h>
+
 
 volatile int toStop = 0;
 
-// 最大并发订阅数
-#define MAX_CONCURRENT_SUBSCRIPTIONS 50
 
 void usage()
 {
@@ -36,257 +78,230 @@ void usage()
     exit(-1);
 }
 
+
+
 void cfinish(int sig)
 {
-    signal(SIGINT, NULL);
-    toStop = 1;
+	signal(SIGINT, NULL);
+	toStop = 1;
 }
+
 
 struct opts_struct
 {
-    char* clientid;
-    int nodelimiter;
-    char* delimiter;
-    enum QoS qos;
-    char* username;
-    char* password;
-    char* host;
-    int port;
-    int showtopics;
-    char* script;
+	char* clientid;
+	int nodelimiter;
+	char* delimiter;
+	enum QoS qos;
+	char* username;
+	char* password;
+	char* host;
+	int port;
+	int showtopics;
+	char* script;
 } opts =
 {
-    (char*)"stdout-subscriber", 0, (char*)"\n", QOS1, NULL, NULL, (char*)"bemfa.com", 9501, 0
+	(char*)"stdout-subscriber", 0, (char*)"\n", QOS1, NULL, NULL, (char*)"bemfa.com", 9501, 0
 };
+
 
 void getopts(int argc, char** argv)
 {
-    int count = 2;
-
-    while (count < argc)
-    {
-        if (strcmp(argv[count], "--qos") == 0)
-        {
-            if (++count < argc)
-            {
-                if (strcmp(argv[count], "0") == 0)
-                {
-                    opts.qos = QOS0;
-                }
-                else if (strcmp(argv[count], "1") == 0)
-                {
-                    opts.qos = QOS1;
-                }
-                else if (strcmp(argv[count], "2") == 0)
-                {
-                    opts.qos = QOS2;
-                    printf("警告：MQTT 支持Qos0 Qos1等级，支持retian保留消息，不支持qos2，使用qos2会被强制下线，次数过多可造成账号异常无法使用。\n");
-                }
-                else
-                {
-                    usage();
-                }
-            }
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--host") == 0)
-        {
-            if (++count < argc)
-                opts.host = argv[count];
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--port") == 0)
-        {
-            if (++count < argc)
-                opts.port = atoi(argv[count]);
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--clientid") == 0)
-        {
-            if (++count < argc)
-                opts.clientid = argv[count];
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--username") == 0)
-        {
-            if (++count < argc)
-                opts.username = argv[count];
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--password") == 0)
-        {
-            if (++count < argc)
-                opts.password = argv[count];
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--delimiter") == 0)
-        {
-            if (++count < argc)
-                opts.delimiter = argv[count];
-            else
-                opts.nodelimiter = 1;
-        }
-        else if (strcmp(argv[count], "--showtopics") == 0)
-        {
-            if (++count < argc)
-            {
-                if (strcmp(argv[count], "on") == 0)
-                {
-                    opts.showtopics = 1;
-                    printf("开启消息主题名显示\n");
-                }
-                else if (strcmp(argv[count], "off") == 0)
-                {
-                    opts.showtopics = 0;
-                }
-                else
-                {
-                    usage();
-                }
-            }
-            else
-                usage();
-        }
-        else if (strcmp(argv[count], "--script") == 0)
-        {
-            if (++count < argc)
-                opts.script = argv[count];
-            else
-                usage();
-        }
-        count++;
-    }
+	int count = 2;
+	
+	while (count < argc)
+	{
+		if (strcmp(argv[count], "--qos") == 0)
+		{
+			if (++count < argc)
+			{
+				if (strcmp(argv[count], "0") == 0)
+				{
+					opts.qos = QOS0;
+				}
+				else if (strcmp(argv[count], "1") == 0)
+				{
+					opts.qos = QOS1;
+				}
+				else if (strcmp(argv[count], "2") == 0)
+				{
+					opts.qos = QOS2;
+					printf("警告：MQTT 支持Qos0 Qos1等级，支持retian保留消息，不支持qos2，使用qos2会被强制下线，次数过多可造成账号异常无法使用。\n");
+				}
+				else
+				{
+					usage();
+				}
+			}
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--host") == 0)
+		{
+			if (++count < argc)
+				opts.host = argv[count];
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--port") == 0)
+		{
+			if (++count < argc)
+				opts.port = atoi(argv[count]);
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--clientid") == 0)
+		{
+			if (++count < argc)
+				opts.clientid = argv[count];
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--username") == 0)
+		{
+			if (++count < argc)
+				opts.username = argv[count];
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--password") == 0)
+		{
+			if (++count < argc)
+				opts.password = argv[count];
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--delimiter") == 0)
+		{
+			if (++count < argc)
+				opts.delimiter = argv[count];
+			else
+				opts.nodelimiter = 1;
+		}
+		else if (strcmp(argv[count], "--showtopics") == 0)
+		{
+			if (++count < argc)
+			{
+				if (strcmp(argv[count], "on") == 0)
+					opts.showtopics = 1;
+				else if (strcmp(argv[count], "off") == 0)
+					opts.showtopics = 0;
+				else
+					usage();
+			}
+			else
+				usage();
+		}
+		else if (strcmp(argv[count], "--script") == 0)
+		{
+			if (++count < argc)
+				opts.script = argv[count];  
+			else
+				usage();
+		}
+		count++;
+	}
+	
 }
+
 
 void messageArrived(MessageData* md)
 {
-    MQTTMessage* message = md->message;
+	MQTTMessage* message = md->message;
 
-    if (opts.showtopics)
-        printf("%.*s\t", md->topicName->lenstring.len, md->topicName->lenstring.data);
-    if (opts.nodelimiter)
-        printf("%.*s", (int)message->payloadlen, (char*)message->payload);
-    else
-        printf("%.*s%s", (int)message->payloadlen, (char*)message->payload, opts.delimiter);
+	if (opts.showtopics)
+		printf("%.*s\t", md->topicName->lenstring.len, md->topicName->lenstring.data);
+	if (opts.nodelimiter)
+		printf("%.*s", (int)message->payloadlen, (char*)message->payload);
+	else
+		printf("%.*s%s", (int)message->payloadlen, (char*)message->payload, opts.delimiter);
+	//fflush(stdout);
 
-    if (opts.script)
-    {
-        char payload_buf[1024];
-        int len = (message->payloadlen < 1023) ? message->payloadlen : 1023;
-        memcpy(payload_buf, message->payload, len);
-        payload_buf[len] = '\0';
+	if (opts.script)
+	{
+    		char payload_buf[1024];
+    		int len = (message->payloadlen < 1023) ? message->payloadlen : 1023;
+   		memcpy(payload_buf, message->payload, len);
+    		payload_buf[len] = '\0'; 
 
-        char command[2048];
-        if (opts.showtopics)
-        {
-            // 格式: <主题名> "<消息>"
-            snprintf(command, sizeof(command), "sh %s \"%.*s\" \"%s\" &",
-                     opts.script,
-                     md->topicName->lenstring.len, md->topicName->lenstring.data,
-                     payload_buf);
-        }
-        else 
-        {
-            // 仅发送消息，不带主题 "<消息>"
-            snprintf(command, sizeof(command), "sh %s \"%s\" &", opts.script, payload_buf);
-        }
+    		char command[2048];
+    		if (opts.showtopics)
+    		{
+        	// 格式: <主题名> "<消息>"
+        	snprintf(command, sizeof(command), "sh %s \"%.*s\" \"%s\" &",
+                 	opts.script,
+                 	md->topicName->lenstring.len, md->topicName->lenstring.data,
+                 	payload_buf);
+    		}
+    		else 
+    		{
+        	// 仅发送消息，不带主题 "<消息>"
+        	snprintf(command, sizeof(command), "sh %s \"%s\" &", opts.script, payload_buf);
+    		}
 
-        system(command);
-    }
+    		system(command);
+	}
 }
 
-void* subscribeTopic(void* topic_param)
-{
-    char* topic = (char*)topic_param;
-    int rc = 0;
-    MQTTClient c;
-    unsigned char buf[100];
-    unsigned char readbuf[100];
-    Network n;
-    NetworkInit(&n);
-    NetworkConnect(&n, opts.host, opts.port);
-    MQTTClientInit(&c, &n, 1000, buf, 100, readbuf, 100);
-
-    MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
-    data.willFlag = 0;
-    data.MQTTVersion = 3;
-    data.clientID.cstring = opts.clientid;
-    data.username.cstring = opts.username;
-    data.password.cstring = opts.password;
-
-    data.keepAliveInterval = 10;
-    data.cleansession = 1;
-
-    rc = MQTTConnect(&c, &data);
-    if (rc != 0)
-    {
-        fprintf(stderr, "连接失败，状态码：%d\n", rc);
-        return NULL;
-    }
-    printf("成功连接到服务器\n");
-
-    rc = MQTTSubscribe(&c, topic, opts.qos, messageArrived);
-    if (rc != 0)
-    {
-        fprintf(stderr, "订阅失败，状态码：%d\n", rc);
-    }
-    else
-    {
-        printf("成功订阅主题: %s\n", topic);
-    }
-
-    while (!toStop)
-    {
-        MQTTYield(&c, 1000);
-    }
-
-    MQTTDisconnect(&c);
-    NetworkDisconnect(&n);
-    return NULL;
-}
 
 int main(int argc, char** argv)
 {
-    int rc = 0;
+	int rc = 0;
+	unsigned char buf[100];
+	unsigned char readbuf[100];
+	
+	if (argc < 2)
+		usage();
+	
+	char* topic = argv[1];
 
-    if (argc < 2)
-        usage();
+	if (strchr(topic, '#') || strchr(topic, '+'))
+	{
+		opts.showtopics = 1;
+		printf("检测到主题名称中包含通配符 # 或 + ，默认开启消息输出主题名。\n");
+	}
+	if (opts.showtopics)
+		printf("主题名是： %s\n", topic);
 
-    char* topic_list = argv[1];
-    char* topics = strdup(topic_list);
+	getopts(argc, argv);	
+	printf("\n状态码：0 表示成功，-1 表示失败。\n\n");
+	Network n;
+	MQTTClient c;
 
-    getopts(argc, argv);
+	signal(SIGINT, cfinish);
+	signal(SIGTERM, cfinish);
 
-    signal(SIGINT, cfinish);
-    signal(SIGTERM, cfinish);
+	NetworkInit(&n);
+	NetworkConnect(&n, opts.host, opts.port);
+	MQTTClientInit(&c, &n, 1000, buf, 100, readbuf, 100);
+ 
+	MQTTPacket_connectData data = MQTTPacket_connectData_initializer;       
+	data.willFlag = 0;
+	data.MQTTVersion = 3;
+	data.clientID.cstring = opts.clientid;
+	data.username.cstring = opts.username;
+	data.password.cstring = opts.password;
 
-    pthread_t threads[MAX_CONCURRENT_SUBSCRIPTIONS];
-    int thread_count = 0;
+	data.keepAliveInterval = 10;
+	data.cleansession = 1;
+	printf("正在连接到 【%s %d】\n", opts.host, opts.port);
+	
+	rc = MQTTConnect(&c, &data);
+	printf("连接状态： %d\n", rc);
+    
+    	printf("正在订阅主题： 【%s】\n", topic);
+	rc = MQTTSubscribe(&c, topic, opts.qos, messageArrived);
+	printf("订阅状态： %d\n", rc);
 
-    char* topic = strtok(topics, ",");
-    while (topic != NULL && thread_count < MAX_CONCURRENT_SUBSCRIPTIONS)
-    {
-        rc = pthread_create(&threads[thread_count], NULL, subscribeTopic, (void*)topic);
-        if (rc != 0)
-        {
-            fprintf(stderr, "创建线程失败，状态码：%d\n", rc);
-            break;
-        }
-        thread_count++;
-        topic = strtok(NULL, ",");
-    }
+	while (!toStop)
+	{
+		MQTTYield(&c, 1000);	
+	}
+	
+	printf("正在关闭...\n");
 
-    for (int i = 0; i < thread_count; i++)
-    {
-        pthread_join(threads[i], NULL);
-    }
+	MQTTDisconnect(&c);
+	NetworkDisconnect(&n);
 
-    free(topics);
-    return 0;
+	return 0;
 }
